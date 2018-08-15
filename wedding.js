d6069797957af27f96f4c415334b6a098d97417d
Carl Steele <casteeleu@gmail.com>
2018-08-15 23:21:52 +0000

    Done.

---
+++ wedding.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+ document.querySelector("[name='firstname'").addEventListener('keypress',callBack)
+ 
+}) 
+function callBack() {
+  // select the firstname input 
+  // get the value of that input
+  // raise an alert with that value
+  alert('danger!')
+}
