f335ac539a21abc763513a149dbfb302439cccda
Jeffrey Jiang <anon0503@gmail.com>
2018-08-18 19:14:02 +0000

    Done.

--- planner.js
+++ planner.js
@@ -4,9 +4,6 @@ function myFunction() {
     var lName = document.getElementById("lastName").value;
     var when = document.getElementById("date").value;
     var place = document.getElementById("location").value;
-    var fInitial = fname.charAt(0);
-    var sInitial = sname.charAt(0);
-    document.getElementById("Initials").innerHTML = fInitial = "&" + sInitial
     document.getElementById("firstSecondname").innerHTML = fName + " & " + sName;
     document.getElementById("lasName").innerHTML = lName;
   	document.getElementById("dateOf").innerHTML = when;
